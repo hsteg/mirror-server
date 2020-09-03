d9f57de79d10e2be51a9ce8239ae265c320f2bd9
Harrison Steg <hstegaa@gmail.com>
2020-09-02 23:53:55 -0400

    add wind direction

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ async function getMtaStatus() {
 
 async function getWeatherNow() {
   try {
-    const url = `https://api.climacell.co/v3/weather/realtime?lat=${process.env.MY_LAT}&lon=${process.env.MY_LONG}&unit_system=us&fields=precipitation%2Ctemp%2Cfeels_like%2Chumidity%2Cwind_speed%2Cwind_direction%2Cwind_gust%2Cprecipitation_type%2Csunrise%2Csunset%2Cvisibility%2Ccloud_cover%2Cmoon_phase%2Cweather_code&apikey=${process.env.CLIMACELL_KEY}`;
+    const url = `https://api.climacell.co/v3/weather/realtime?lat=${process.env.MY_LAT}&lon=${process.env.MY_LONG}&unit_system=us&fields=precipitation%2Ctemp%2Cfeels_like%2Chumidity%2Cwind_speed%2Cwind_direction%2Cwind_gust%2Cprecipitation_type%2Csunrise%2Csunset%2Cvisibility%2Ccloud_cover%2Cmoon_phase%2Cweather_code%2Cepa_health_concern&apikey=${process.env.CLIMACELL_KEY}`;
     const response = await axios.get(url);
 
     return response.data;
